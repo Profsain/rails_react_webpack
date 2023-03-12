9f822eb68cea08ed51104f5a7e8c7cf38e933054
Profsain <profsaincrown@gmail.com>
2023-03-12 08:24:36 +0000

    addd changes required

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  get '/random_greeting', to: 'greetings#random_greeting'
+  namespace :api do
+    namespace :v1 do
+      resources :greetings, only: [:index]
+    end
+  end
+  root 'root#index'
 end
